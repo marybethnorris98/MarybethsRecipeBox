6bde200a2105c6a4f451aee487fbc799b03a96dd
marybethnorris98 <marybeth.norris@mcgeheeschools.org>
2025-11-24 10:24:20 -0600

    Update script.js version 20

--- script.js
+++ script.js
@@ -543,7 +543,7 @@ document.addEventListener("DOMContentLoaded", () => {
   saveDraftBtn.style.borderRadius= "12px";
   saveDraftBtn.style.width= "100%";
   saveDraftBtn.style.cursor= "pointer";
-  saveDraftBtn.style.marginBottom = "10px";
+  saveDraftBtn.style.marginBottom = "40px";
   
 
   // Attach the function (we’ll add this function next)
