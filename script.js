3507146a61f4ead7cd9eb7db63511c2953e85c92
marybethnorris98 <marybeth.norris@mcgeheeschools.org>
2025-11-27 09:36:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -130,6 +130,9 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
     draftsList = document.getElementById("draftsList");
 
+    const defaultCloseBtn = draftsModal.querySelector(".close") || draftsModal.querySelector("#closeViewerBtn");
+if (defaultCloseBtn) defaultCloseBtn.style.display = 'none';
+
     // --- Apply Styles ---
     if (saveRecipeBtn) {
         Object.assign(saveRecipeBtn.style, {
